ea9f3c9f9adc5cd6e4bd11d96351a855b63e83ad
GabrieleFerrero <gabriele.ferrero@polito.it>
2023-09-20 11:05:52 -0400

    Test max

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,28 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_daily_max_zeros():
+    """Test that mean function works for an array of zeros."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[0, 0],
+                           [0, 0],
+                           [0, 0]])
+    test_result = np.array([0, 0])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+
+def test_daily_max_integers():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[1, 2],
+                           [5, 4],
+                           [3, 6]])
+    test_result = np.array([5, 6])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_max(test_input), test_result)
